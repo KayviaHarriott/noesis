c5d68e4eac8dc02bab8850942d1eaca460f788bd
Kayvia Harriott <kayviaharriott@hotmail.com>
2025-10-18 15:58:11 -0500

    Removed unused variable

--- noesis-web/src/pages/AgentDashboard.tsx
+++ noesis-web/src/pages/AgentDashboard.tsx
@@ -5,7 +5,6 @@ import {
   fetchAiSuggestion,
   fetchEmotion,
 } from "../api/agentApi";
-import { Link } from "react-router-dom";
 
 export const AgentDashboard = () => {
   // Chat + AI suggestion
